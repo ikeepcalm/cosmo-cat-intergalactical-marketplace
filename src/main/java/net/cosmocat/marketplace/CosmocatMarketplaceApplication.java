1455d3fdb975f47e84e261ed5e03e48860d698c6
ikeepcalm <horokhbohdandmytrovich@gmail.com>
2025-09-24 09:41:23 +0200

    Init commit.

---
+++ src/main/java/net/cosmocat/marketplace/CosmocatMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package net.cosmocat.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmocatMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmocatMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/cosmocat/marketplace/CosmocatMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cosmocat.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmocatMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

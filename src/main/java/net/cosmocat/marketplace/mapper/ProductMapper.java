e0ec9c6640fd13df8bc9ec2253176c9caba7c786
ikeepcalm <horokhbohdandmytrovich@gmail.com>
2025-10-26 13:19:55 +0200

    Disable DataSourceAutoConfiguration (crying for database provider without H2)

--- src/main/java/net/cosmocat/marketplace/mapper/ProductMapper.java
+++ src/main/java/net/cosmocat/marketplace/mapper/ProductMapper.java
@@ -16,8 +16,16 @@ public interface ProductMapper {
 
   List<ProductDTO> toDTOList(List<Product> products);
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "createdAt", ignore = true)
+  @Mapping(target = "updatedAt", ignore = true)
+  @Mapping(target = "category", ignore = true)
   Product toEntity(ProductCreateDTO request);
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "createdAt", ignore = true)
+  @Mapping(target = "updatedAt", ignore = true)
+  @Mapping(target = "category", ignore = true)
   @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
   void updateEntityFromRequest(ProductUpdateDTO request, @MappingTarget Product product);
 }
